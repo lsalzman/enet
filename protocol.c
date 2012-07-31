f6160fd2e93138610f4fe5a514b2351d2e2ee998
Lee Salzman <lsalzman@gmail.com>
2012-07-31 16:12:11 +0300

    case warning fixes

--- protocol.c
+++ protocol.c
@@ -83,6 +83,9 @@ enet_protocol_dispatch_incoming_commands (ENetHost * host, ENetEvent * event)
            }
 
            return 1;
+
+       default:
+           break;
        }
     }
 
@@ -876,6 +879,9 @@ enet_protocol_handle_acknowledge (ENetHost * host, ENetEvent * event, ENetPeer *
            enet_list_empty (& peer -> outgoingUnreliableCommands) &&   
            enet_list_empty (& peer -> sentReliableCommands))
          enet_peer_disconnect (peer, peer -> eventData);
+       break;
+
+    default:
        break;
     }
    
