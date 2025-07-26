a2701dcd28f1f19aac201821e7d4d82ee050a8e8
Chris Trenkamp <chris@christrenkamp.com>
2025-07-26 08:07:30 -0400

    The build system now checks for the C offsetof macro, and is expanded into a new enet macro for checking the offset of a field.

--- include/enet/utility.h
+++ include/enet/utility.h
@@ -5,9 +5,19 @@
 #ifndef __ENET_UTILITY_H__
 #define __ENET_UTILITY_H__
 
+#ifdef HAS_OFFSETOF
+#include <stddef.h>
+#endif
+
 #define ENET_MAX(x, y) ((x) > (y) ? (x) : (y))
 #define ENET_MIN(x, y) ((x) < (y) ? (x) : (y))
 #define ENET_DIFFERENCE(x, y) ((x) < (y) ? (y) - (x) : (x) - (y))
+
+#ifdef HAS_OFFSETOF
+#define ENET_OFFSETOF(str, field) (offsetof(str, field))
+#else
+#define ENET_OFFSETOF(str, field) ((size_t) & ((str *) 0) -> field)
+#endif
 
 #endif /* __ENET_UTILITY_H__ */
 
--- protocol.c
+++ protocol.c
@@ -1021,7 +1021,7 @@ enet_protocol_handle_incoming_commands (ENetHost * host, ENetEvent * event)
     enet_uint16 peerID, flags;
     enet_uint8 sessionID;
 
-    if (host -> receivedDataLength < offsetof(ENetProtocolHeader, sentTime))
+    if (host -> receivedDataLength < ENET_OFFSETOF(ENetProtocolHeader, sentTime))
       return 0;
 
     header = (ENetProtocolHeader *) host -> receivedData;
@@ -1031,7 +1031,7 @@ enet_protocol_handle_incoming_commands (ENetHost * host, ENetEvent * event)
     flags = peerID & ENET_PROTOCOL_HEADER_FLAG_MASK;
     peerID &= ~ (ENET_PROTOCOL_HEADER_FLAG_MASK | ENET_PROTOCOL_HEADER_SESSION_MASK);
 
-    headerSize = (flags & ENET_PROTOCOL_HEADER_FLAG_SENT_TIME ? sizeof (ENetProtocolHeader) : offsetof(ENetProtocolHeader, sentTime));
+    headerSize = (flags & ENET_PROTOCOL_HEADER_FLAG_SENT_TIME ? sizeof (ENetProtocolHeader) : ENET_OFFSETOF(ENetProtocolHeader, sentTime));
     if (host -> checksum != NULL)
       headerSize += sizeof (enet_uint32);
 
@@ -1683,7 +1683,7 @@ enet_protocol_send_outgoing_commands (ENetHost * host, ENetEvent * event, int ch
             host -> buffers -> dataLength = sizeof (ENetProtocolHeader);
         }
         else
-          host -> buffers -> dataLength = offsetof(ENetProtocolHeader, sentTime);
+          host -> buffers -> dataLength = ENET_OFFSETOF(ENetProtocolHeader, sentTime);
 
         shouldCompress = 0;
         if (host -> compressor.context != NULL && host -> compressor.compress != NULL)
